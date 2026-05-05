4b21c4b3af721edfdd8acf541961fed0bd50cff9
legendasoftltd-create <legendasoftltd@gmail.com>
2026-05-05 08:02:59 +0200

    first commit

---
+++ config/system-addons.php
@@ -0,0 +1,53 @@
+<?php return array (
+  'admin_panel' => 
+  array (
+    'active' => '0',
+    'username' => 'admin',
+    'purchase_key' => 'sdfdsfdsf',
+    'software_id' => 'MzY3NzIxMTI=',
+    'domain' => 'am.example.com',
+    'software_type' => 'product',
+  ),
+  'vendor_panel' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'user_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'vendor_app' => 
+  array (
+    'active' => 0,
+    'username' => 'admin',
+    'software_id' => 'MzY3NzIxNzM=',
+    'domain' => 'am.example.com',
+    'software_type' => 'addon',
+  ),
+  'deliveryman_app' => 
+  array (
+    'active' => 0,
+    'username' => 'admin',
+    'software_id' => 'MzY3NzIxNDg=',
+    'domain' => 'am.example.com',
+    'software_type' => 'addon',
+  ),
+  'react_web' => 
+  array (
+    'active' => 0,
+    'username' => 'admin',
+    'software_id' => 'NDUzNzAzNTE=',
+    'domain' => 'bs.sobjabe.com',
+    'software_type' => 'addon',
+  ),
+);
